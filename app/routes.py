cc9fe1919281ae8677d0635f1a303d9e101f1e48
Ubuntu <ubuntu@ip-172-31-90-80.ec2.internal>
2018-09-13 21:00:53 +0000

    demo3a

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -17,4 +18,11 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+if __name__ == "__main__":
+	app.run()
 
