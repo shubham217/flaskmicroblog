06d3c85c9104a8b288ddfa4600a2da6a29385ad5
Ubuntu <ubuntu@ip-172-31-90-80.ec2.internal>
2018-09-13 19:52:16 +0000

    Demo2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
 
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'shubham'}
+    posts = [
+        {
+            'author': {'username': 'John Cena'},
+            'body': 'New season of flash starting today!'
+        },
+        {
+            'author': {'username': 'Iron man'},
+            'body': 'I am the strongest'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
